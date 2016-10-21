3f170b0005dfa2ca99e05dadde00b74678ea271a
yangyandd <yangyandd@gmail.com>
2016-10-21 01:05:15 -0400

    insert_item.php

---
+++ add_modify_yy/insert_item.php
@@ -0,0 +1,51 @@
+<?php
+
+require('db_cn.inc');
+
+require('item_insert_result_ui.inc');
+
+insert_item();
+
+function insert_item(){
+
+connect_and_select_db(DB_SERVER, DB_UN, DB_PWD， DB_NAME);
+
+   $itemname = $_POST['itemname'];
+
+
+   $insertitem = "INSERT INTO InventoryItem();"
+
+   $result = mysql_query($insertitem);
+   echo $result;
+   $message = "";
+
+   if(!$result){
+
+   $message = "Error in inserting item: $itemname". mysql_error();
+   
+   }
+   else{
+
+   $message = "Data for User: $itemname inserted successfully.";
+   
+   }
+
+   ui_show_item_insert_result($message, $itemname, $result);
+}
+   function connect_and_select_db($server, $username, $pwd, $dbname)
+   {
+   	// Connect to db server
+   	$conn = mysql_connect($server, $username, $pwd);
+   	if (!$conn) {
+   	    echo "Unable to connect to DB: " . mysql_error();
+       	    exit;
+   	}
+   	// Select the database
+   	$dbh = mysql_select_db($dbname);
+   	if (!$dbh){
+       		echo "Unable to select ".$dbname.": " . mysql_error();
+   		exit;
+   	}
+   }
+
+?>
