3d6810d146ed0c8c1b5489c0678be1c64066d1e2
Ryan Tampone <rtamp1@brockport.edu>
2016-10-21 21:54:13 -0400

    Updates to remove vendor
    Now has a confirm screen before deleting

---
+++ Working Vendor Files/Working Delete Files/delete_vendor_ui_form.php
@@ -0,0 +1,28 @@
+<?php
+	include 'header.php';
+?>
+
+<?php
+	echo"
+		<br>
+		<div id='callToAction'>
+	    <h4 align='center'>Please Enter the Vendor ID of the Vendor to Delete</h4>
+	  </div>
+    	<div id='userdataform'>
+            <form action='delete_vendor.php' method='post'>
+                <table align='center'>
+                    <tr>
+                        <td><span align='right'>Enter Vendor ID:</span></td>
+                        <td><input NAME='vendorid' TYPE='text' SIZE='50' required/></td>
+                    </tr>
+                </table>
+                <p align='center'>
+                    <input type='submit' value='Submit'/>
+                    <input type='reset' value='Reset'/>
+                </p>
+            </form>
+        </div>
+	";
+?>
+</body>
+</html>
