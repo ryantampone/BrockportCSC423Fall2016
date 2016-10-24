3a6e0e261e7022a45dd7cb297e6fd737e41085c3
yangyandd <yangyandd@gmail.com>
2016-10-24 16:57:36 -0400

    Update delete_item.php

--- add_modify_yy/delete_item.php
+++ add_modify_yy/delete_item.php
@@ -24,7 +24,7 @@ function delete_item()
 	// Create a String consisting of the SQL command. Remember that
         // . is the concatenation operator. $varname within double quotes
  	// will be evaluated by PHP
-	$sql_stmt = "DELETE * FROM InventoryItem WHERE ItemId='$itemId';";
+	$sql_stmt = "DELETE FROM InventoryItem WHERE ItemId='$itemId';";
 	//Execute the query. The result will just be true or false
 	$result = mysql_query($sql_stmt);
 	echo $result;
