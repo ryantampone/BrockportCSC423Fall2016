83a67935a037027abca951fa3b7b0cf60c5ee2ad
yangyandd <yangyandd@gmail.com>
2016-10-26 22:44:39 -0400

    Update new_item_ui_form.php

--- new_item_ui_form.php
+++ new_item_ui_form.php
@@ -14,11 +14,11 @@
       	<table align='center'>
         	<tr>
           	<td align='right'>Item ID:</td>
-            <td><input id='ItemId' name='ItemId' TYPE='text' SIZE='50' onKeyPress='return hasToBeNumber(event)' onpaste='return false' required/></td>
+            <td><input id='ItemId' name='ItemId' TYPE='text' SIZE='50' onKeyPress='return hasToBeNumber(evt)' onpaste='return false' required/></td>
           </tr>
           <tr>
             <td align='right'>Description:</td>
-            <td><input id='Description' name='Description' type='textarea' onKeyPress='return anythingButQuotes(event)' onpaste='return false' required/></td>
+            <td><input id='Description' name='Description' type='textarea' onKeyPress='return anythingButQuotesOrSlash(evt)' onpaste='return false' required/></td>
           </tr>
           <tr>
             <td align='right'>Size:</td>
@@ -28,8 +28,8 @@
             <td align='right'>Division:</td>
 						<td>
 							<select name='Division'>
-								<option value='food_convenience'>Food Convenience</option>
-								<option value='meat_convenience'>Meat Convenience</option>
+								<option value='Food Convenience'>Food Convenience</option>
+								<option value='Meat Convenience'>Meat Convenience</option>
 							</select>
 						</td>
           </tr>
@@ -37,11 +37,11 @@
             <td align='right'>Department:</td>
 						<td>
 							<select name='Department'>
-								<option value='meat_department'>Meat Department</option>
-								<option value='beverages_department'>Beverages Department</option>
-								<option value='frozen_foods_department'>Frozen Foods Department</option>
-								<option value='produce_department'>Produce Department</option>
-								<option value='bread/bakery_department'>Bread/Bakery Department</option>
+								<option value='Meat Department'>Meat Department</option>
+								<option value='Beverages Department'>Beverages Department</option>
+								<option value='Frozen Foods Department'>Frozen Foods Department</option>
+								<option value='Produce Department'>Produce Department</option>
+								<option value='Bread/Bakery Department'>Bread/Bakery Department</option>
 							</select>
 						</td>
 					</tr>
@@ -49,20 +49,20 @@
               <td align='right'>Category:</td>
               <td>
 								<select name='Category'>
-										<option value='beverages'>Beverages</option>
-										<option value='bread/bakerty'>Bread/Bakery</option>
-										<option value='meat'>Meat</option>
-										<option value='frozen foods'>Frozen Foods</option>
+										<option value='Beverages'>Beverages</option>
+										<option value='Bread/Bakery'>Bread/Bakery</option>
+										<option value='Meat'>Meat</option>
+										<option value='Frozen Foods'>Frozen Foods</option>
 								</select>
 							</td>
           </tr>
 					<tr>
 							<td align='right'>Item Cost:</td>
-							<td><input id='ItemCost' name='ItemCost' TYPE='text' onKeyPress='return isPrice(event)' onpaste='return false' SIZE='50' required/></td>
+							<td><input id='ItemCost' name='ItemCost' TYPE='text' onKeyPress='return isPrice(evt)' onpaste='return false' SIZE='50' required/></td>
 					</tr>
 					<tr>
 							<td align='right'>Item Retail:</td>
-							<td><input id='ItemRetail' name='ItemRetail' TYPE='text' SIZE='50' onKeyPress='return isPrice(event)' onpaste='return false' required/></td>
+							<td><input id='ItemRetail' name='ItemRetail' TYPE='text' SIZE='50' onKeyPress='return isPrice(evt)' onpaste='return false' required/></td>
 					</tr>
 					<tr>
 							<td align='right'>Vendor Id:</td>
@@ -70,7 +70,7 @@
 					</tr>
 					<tr>
 							<td align='right'>Image Filename:</td>
-							<td><input id='ImageFileName' name='ImageFileName' TYPE='text' SIZE='50' onKeyPress='return isImageFileName(event)' onpaste='return false'   value='BrockportCSC423Fall2016/src/' /></td>
+							<td><input id='ImageFileName' name='ImageFileName' TYPE='text' SIZE='50' onKeyPress='return isImageFileName(evt)' onpaste='return false'   value='BrockportCSC423Fall2016/src/' /></td>
 					</tr>
 					</table>
 					<div id='button'>
