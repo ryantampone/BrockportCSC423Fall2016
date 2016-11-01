efbbd1d6fbb3669947f183bf55fa232a90f34a32
Ryan Tampone <rtamp1@brockport.edu>
2016-10-31 22:04:47 -0400

    Adjust CSS

--- indexVendor.php
+++ indexVendor.php
@@ -3,13 +3,10 @@
 ?>
 
 <div id="callToAction">
-  <h2>Please Select an Option From the Navigation Bar</h2>
+  <h2>Please Login Below with Your Vendor ID and Password</h2>
 </div>
 
-<br><br><br>
-<h3 align='center'>(Something Cool is Coming Here)</h3>
 
-<p align='center'><img src='/~rtamp1/csc423/gp/src/gift.png' class="img_m"/></p>
 
 </body>
 </html>
