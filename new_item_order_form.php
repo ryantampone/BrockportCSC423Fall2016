7fa7cc4d020bdc21095579dacb7e6d76e82f6579
yangyandd <yangyandd@gmail.com>
2016-11-11 10:28:44 -0500

    Update new_item_order_form.php

--- new_item_order_form.php
+++ new_item_order_form.php
@@ -20,26 +20,26 @@
 ?>
 
 <?php
+     $vendorid;
+
 
 	echo"
 		<br>
 		<h2>Please fill out the new order form below.</h2>
 		<form action='new_order.php' method='post'>
 			<table align='center'>
-			<tr>
-				<td><span align='right'>Order ID:</span></td>
-				<td><input type='text' id='OrderId' name='OrderId' size='50' /></td>
-			</tr>
+		
 			<tr>
 				<td ><span align='right'>Vendor ID:</span></td>
 				
 				<td>
-								<select id='VendorId' name='VendorId' required/>
+								<select id='VendorInfo'name='VendorInfo' />
 							";
+							       
+									$next_option=
 									connect_and_select_db(DB_SERVER, DB_UN, DB_PWD,DB_NAME);
 									$sql_vendors = "SELECT VendorId, VendorName FROM Vendor WHERE Status='Active';";
 									$vendors_result = mysql_query($sql_vendors);
-
 									if (!$vendors_result)
 									{
 										echo "VendorId's retrieved unsuccessfully: ".mysql_error();
@@ -48,43 +48,17 @@
 									while ($row = mysql_fetch_assoc($vendors_result))
 									{
 										$vendorid = $row['VendorId'];
-										$vendorname = mysql_real_escape_string($row['VendorName']);
-										echo "<option>".$vendorid.": ".$vendorname."</option>";
+										$vendorname = $row['VendorName'];
+										echo "<option>".$vendorid." : ".$vendorname."</option>";
 									}
+								
 			  echo "</select>
-						</td>
+	
+			</td>
 				
 			</tr>
-			<tr>
-					<td><span align='right'>Store ID:</span></td>
-					<td>
-					<select id='StoreId' name='StoreId' required />";
-					$sql_store = "SELECT StoreName FROM RetailStore WHERE Status='Active';";
-					$store_result = mysql_query($sql_store);
-					if (!$store_result)
-					{
-						echo "Store's retrieved unsuccessful: ".mysql_error();
-						exit;
-						
-						}	
-					while($row = mysql_fetch_assoc($store_result)){
-						
-						$storeid = $row['StoreId'];
-						$storename = mysql_real_escape_string($row['StoreName']);
-						echo "<option>".$storeid.": ".$storename."</option>";
-						}
-					echo "<option value='$vendorId'>$vendorId</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td ><span align='right'>Date:</span></td>
-				<td><input type='date'  id='DateTimeOfOrder' name='DateTimeOfOrder' required /></td>
-			</tr>
-			<tr>
-				<td><span align='right'>Status:</span></td>
-				<td><input type='text' id='Status' name='Status' value='Pending' /></td>
-			</tr>
+			
+			
 			
 			</table>
 		<div class='button'>
@@ -96,4 +70,3 @@
 ?>
 
 </body>
-</html>
