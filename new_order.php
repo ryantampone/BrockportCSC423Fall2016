827f9727bc41aa3945764106172f69a315e7f548
yangyandd <yangyandd@gmail.com>
2016-11-15 18:13:16 -0500

    Update new_order.php

--- new_order.php
+++ new_order.php
@@ -3,14 +3,6 @@ require('db_cn.inc');
 include 'header.php';
 require('insert_order.php');
 
-/*$vendorinfo = $_POST['VendorInfo'];
-$vendorid = split(':',$vendorinfo);
-$vendorid = $vendorid[0];
-echo "$vendorid";
-$vendorID = intval($vendorid);
-echo "$vendorID";
-*/
-//$id_for_next_qty;
 
 
 
@@ -24,13 +16,9 @@ show_vendor_item();
 
 function show_vendor_item(){
 	connect_and_select_db(DB_SERVER, DB_UN, DB_PWD,DB_NAME);
-	/*$vendorinfo = $_POST['VendorInfo'];
-	$vendorid = split(':',$vendorinfo);
-	$vendorid = $vendorid[0];
-	$vendorid = $_POST['vendorId'];
-	echo $vendorid;*/
+
 	$vendorName = $_POST['VendorInfo'];
-	//echo"$vendorName";
+
 	connect_and_select_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
 	$sql_vendorId = "SELECT VendorId FROM `Vendor` WHERE VendorName='$vendorName';";
 	$sql_result = mysql_query($sql_vendorId);
@@ -38,8 +26,8 @@ function show_vendor_item(){
 	{
 		$vendorId = $vendor_row['VendorId'];
 	}
-	echo"My vendorId:";
-	echo "$vendorId";
+	//echo"My vendorId:";
+	//echo "$vendorId";
 	echo"<input type='hidden' id='myvendorid' name='myvendorid' value='$vendorId' />";
 	
 	
@@ -53,14 +41,7 @@ function show_vendor_item(){
 			exit;
 		}
 
-	/*$row = mysql_num_rows($result_showItem);
-
-	$message="";
-	if($row ==0){
-		
-		$message = "No item found!";
-		}
-	*/
+	
 	show_item($message, $result_showItem, $vendorId);
 	mysql_free_result($result_showItem);
 	}
