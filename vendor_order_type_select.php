42ad6f90ee39f9fea9072fd03e312cb634919421
Ryan Tampone <rtamp1@brockport.edu>
2016-11-17 18:37:56 -0500

    Fixes

--- vendor_order_type_select.php
+++ vendor_order_type_select.php
@@ -46,7 +46,7 @@ if (isset($_SESSION['VendorCode']))
 else
 {
 	echo ("<SCRIPT LANGUAGE='JavaScript'>
-	window.alert('Please Login to View This Page')
+	window.alert('Invalid Login Credentials: Please Login to View This Page')
 	window.location.href='http://www.itss.brockport.edu/~rtamp1/csc423/gp/indexVendor.php';
 	</SCRIPT>");
 	//echo "<script type='text/javascript'>alert('Please login to view this page') window.location.href='./indexVendor.php'</script>";
