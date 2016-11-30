7c6104fe9381570de27157cba013c30a825ce479
yangyandd <yangyandd@gmail.com>
2016-11-29 22:51:56 -0500

    Update report_all_items.php

--- report_all_items.php
+++ report_all_items.php
@@ -1,7 +1,6 @@
 <?php
 	include 'header.php';
 	require('db_cn.inc');
-
 	function connect_and_select_db($server, $username, $pwd, $dbname)
 {
 	// Connect to db server
@@ -21,22 +20,16 @@
 
 <?php
      $storeName;
-
-
 	echo"
 		<br>
 		<h2>Total Inventory Report : Please select a store location</h2>
 		<form action='report_all_item_list.php' method='post'>
 			<table align='center'>
-
 			<tr>
 				<td ><span align='right'>Store ID:</span></td>
-
 				<td>
 								<select id='StoreName' name='StoreName' >
 							";
-
-
 									connect_and_select_db(DB_SERVER, DB_UN, DB_PWD,DB_NAME);
 									$sql_storeLocations = "SELECT StoreName FROM RetailStore;";
 									$StoreName_result = mysql_query($sql_storeLocations);
@@ -47,35 +40,22 @@
 									}
 									while ($row = mysql_fetch_assoc($StoreName_result))
 									{
-
 										$storeName = $row['StoreName'];
 										echo "<option>".$storeName."</option>";
 									}
-
 			  echo "</select>";
-
 			  	 /*$vendorinfo = $_POST['VendorInfo'];
 				$vendorid = split(':',$vendorinfo);
 				$vendorid = $vendorid[0];
 				echo "$vendorid";
 			  	echo "<input type='hidden' id='vendorId' name='vendorId' value='$vendorid' />";*/
-
-
 			  echo"
-
-
-
 			</td>
-
 			</tr>
-
-
-
 			</table>
 		<div class='button'>
 						<input id='tiny_button' type='submit' id='submit' name='submit' >
 						<input id='tiny_button' type='reset' id='reset' name='reset'>
 					</div>
 		</form>";
-
 ?>
