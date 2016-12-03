a01d63f84580b1794390e3ec6f52c2e1016ffca4
yangyandd <yangyandd@gmail.com>
2016-12-03 12:26:09 -0500

    Update report_delivered_item_list.php

--- report_delivered_item_list.php
+++ report_delivered_item_list.php
@@ -6,123 +6,102 @@ include 'header.php';
 
 
 $storeName = mysql_escape_string($_POST['StoreName']);
+//echo "Store Name: ".$storeName;
+//echo "<br />";
+$startDate = $_POST['startDate'];
+//echo "Start Date: ".$startDate;
+//echo "<br />";
+$endDate = $_POST['endDate'];
+//echo "End Date: ".$endDate;
+//echo "<br />";
 
 
-show_item($storeName);
-function show_item($storeName){
+show_item_delivered($storeName, $startDate, $endDate);
+function show_item_delivered($storeName, $startDate, $endDate){
 			//echo "StoreName is ".$storeName."";
 			connect_and_select_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
-			$sql_showItem = "SELECT ItemId, QuantityInStock FROM Inventory WHERE QuantityInStock >'10' AND StoreId in (SELECT StoreId FROM RetailStore WHERE StoreName = '$storeName');";
-			$result = mysql_query($sql_showItem);
-			if (!$result)
+			
+			
+	
+			
+			//find the orderID which is matching the storename and the date
+			$sql_orderId = "SELECT OrderId, DateTimeOfOrder FROM `Order` WHERE ((`DateTimeOfOrder` > '$startDate') AND (`DateTimeOfOrder` < '$endDate')) AND StoreId in (SELECT StoreId FROM `RetailStore` WHERE StoreName ='Long Pond Road Greece');";
+
+
+			$resule_orderId = mysql_query($sql_orderId);
+			if (!$resule_orderId)
 				  {
 					 echo "The retrieval was unsuccessful: ".mysql_error();
 					 exit;
 				  }
-		      
+			
 			  
 			  echo"
-			  <h2>Item Inventory Report</h2>
-			<form action='export_item.php' method='post'>
+			  <h2>Item Delivered Report</h2>
+			<form action='export_delivered.php' method='post'>
 			<table align='center'>
 					<tr>
+						<th style=\"padding-right: 15px;\" align='center'><u>Order ID</u></th>
 						<th style=\"padding-right: 15px;\" align='center'><u>Item ID</u></th>
 						<th style=\"padding-right: 15px;\" align='center'><u>Item Name</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Item Size</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Division</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Department</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Category</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Item Cost</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Item Retail</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Image File Name</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Vendor ID</u></th>
-						<th style=\"padding-right: 15px;\" align='center'><u>Quantity</u></th>
+						<th style=\"padding-right: 15px;\" align='center'><u>DateTimeOfOrder</u></th>
+					
 					</tr>
 		 		";
 				$count = 0;
+				$countO = 0;
 				$array = 0;
-				 while ($row = mysql_fetch_assoc($result))
+				 while ($row = mysql_fetch_assoc($resule_orderId))
 			  		{		
 					 
-						 $itemid = $row['ItemId'];
-						 $qty = $row['QuantityInStock'];
-						 $qty_sql = "SELECT * FROM `InventoryItem` WHERE ItemId='$itemid';";
-						 $item_result = mysql_query($qty_sql);
+						  $orderId = $row['OrderId'];
+						 // $orderId_ex = 'orderId'.$count;
+						  $dateTimeOfOrder = $row['DateTimeOfOrder'];
+						// $dateTimeOfOrder_ex = 'dateTimeOfOrder'.$count;
+						 $countO ++;
+						  
+						 $sql_itemInfo = "SELECT ItemId, Description FROM InventoryItem WHERE ItemId in (SELECT ItemId FROM OrderDetail WHERE OrderId = '$orderId');";
+						 $item_result = mysql_query($sql_itemInfo);
 						 while($qty_row = mysql_fetch_assoc($item_result)){
-							 $description =$qty_row['Description'];
-							 $size = $qty_row['Size'];
-							 $division = $qty_row['Division'];
-							 $department = $qty_row['Department'];
-							 $category = $qty_row['Category'];
-							 $itemCost = $qty_row['ItemCost'];
-							 $itemRetail = $qty_row['ItemRetail'];
-							 $imageFileName =$qty_row['ImageFileName'];
-							 $vendorId = $qty_row['VendorId'];
-						    //$description = $qty_row['Description'];
-						 	
+						 
+						 	$itemId = $qty_row['ItemId'];
+							$description = $qty_row['Description'];
 							$count++;
 							//echo"count is ".$count."\n";
 							$array++;
 							//echo"array count is ".$array."\n";
-							$itemid_ex = 'itemid_ex'.$count;
-						
+							$itemId_ex = '$itemId_ex'.$count;
+							//echo"itemid is ".$itemid_ex."\n";
 							$description_ex = 'description_ex'.$count;
-							
-							$size_ex = 'size_ex'.$count;
-							$division_ex = 'division_ex'.$count;
-							$department_ex = 'department_ex'.$count;
-							$category_ex = 'category_ex'.$count;
-							$itemCost_ex = 'itemCost_ex'.$count;
-							$itemRetail_ex = 'itemRetail_ex'.$count;
-							$imageFileName_ex = 'imageFileName_ex'.$count;
-							$vendorId_ex = 'vendorId_ex'.$count;
-							
-							$qty_ex = 'qty_ex'.$count;
-						
+							//echo"description is ".$description_ex."\n";
+							$dateTimeOfOrder_ex = 'dateTimeOfOrder'.$count;
+							$orderId_ex = 'orderId'.$count;
+							//echo"qty is ".$qty_ex."\n";
 							
 							 echo"
 								<tr>
-								<td><p style=\"padding-right: 15px;\">".$itemid."</p></td>
-								<input type='hidden' name='$itemid_ex' value='$itemid' />
+								
+								<td><p style=\"padding-right: 15px;\">".$orderId."</p></td>
+								<input type='hidden' name='$orderId_ex' value='$orderId' />
+								
+								<td><p style=\"padding-right: 15px;\">".$itemId."</p></td>
+								<input type='hidden' name='$itemId_ex' value='$itemId' />
 								
 								<td><p style=\"padding-right: 15px;\">".$description."</p></td>
 								<input type='hidden' name='$description_ex' value='$description' />
+								<td><p style=\"padding-right: 15px;\">".$dateTimeOfOrder."</p></td>
+								<input type='hidden' name='$dateTimeOfOrder_ex' value='$dateTimeOfOrder' />
 								
-								<td><p style=\"padding-right: 15px;\">".$size."</p></td>
-								<input type='hidden' name='$size_ex' value='$size' />
-								
-								<td><p style=\"padding-right: 15px;\">".$division."</p></td>
-								<input type='hidden' name='$division_ex' value='$division' />
-								
-								<td><p style=\"padding-right: 15px;\">".$department."</p></td>
-								<input type='hidden' name='$department_ex' value='$department' />
-								
-								<td><p style=\"padding-right: 15px;\">".$category."</p></td>
-								<input type='hidden' name='$category_ex' value='$category' />
-								
-								<td><p style=\"padding-right: 15px;\">".$itemCost."</p></td>
-								<input type='hidden' name='$itemCost_ex' value='$itemCost' />
-								
-								<td><p style=\"padding-right: 15px;\">".$itemRetail."</p></td>
-								<input type='hidden' name='$itemRetail_ex' value='$itemRetail' />
-								
-								<td><p style=\"padding-right: 15px;\">".$imageFileName."</p></td>
-								<input type='hidden' name='$imageFileName_ex' value='$imageFileName' />
-								
-								<td><p style=\"padding-right: 15px;\">".$vendorId."</p></td>
-								<input type='hidden' name='$vendorId_ex' value='$vendorId' />
-								
-								<td><p style=\"padding-right: 15px;\">".$qty."</p></td>
-								
-								<input type='hidden' name='$qty_ex' value='$qty' />
 								</tr>
 								
 								";
 						
 						 }
 						 
+						 
 				
 				}
+				$array = $count + $countO;
 			echo"
 						<input type='hidden' name='count' value='$count' />
 						<input type='hidden' name='array' value='$array' />
