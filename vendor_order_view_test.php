452a762740b418f8d67a1d6d7dc0a7108e95f550
yangyandd <yangyandd@gmail.com>
2016-12-08 17:19:17 -0500

    Update vendor_order_view_test.php

--- vendor_order_view_test.php
+++ vendor_order_view_test.php
@@ -1,8 +1,10 @@
 <?php
 	include 'headerVendor.php';
+	
 ?>
 <?php
-
+ echo"
+   <h2>All Pending Orders</h2>";
 function show_order($result, $orderStatus)
 {
   //----------------------------------------------------------
@@ -11,12 +13,9 @@ function show_order($result, $orderStatus)
   echo "<HEAD>";
   echo "</HEAD>";
   echo "<BODY>";
-
-
    //While there are more rows in the $result, get the next row
    //as an associative array
-   echo"
-   <h2>All Pending Orders</h2>";
+  
    while ($row = mysql_fetch_assoc($result))
    {
 		 $orderid = $row['OrderId'];
@@ -25,42 +24,57 @@ function show_order($result, $orderStatus)
 		 $orderdatetime = $row['DateTimeOfOrder'];
 		 $status = $row['Status'];
 		 $completedatetime = $row['DateTimeOfFulfillment'];
-
+		 
+		 
 		 echo"
 	 		<div id='callToAction'>
 				<br>
+	       <form action='view_result_ui.php' method='post'>
+				<input type='hidden' name='pop_storeid' value='$storeid' />
+				<input type='hidden' name='pop_status' value='$status' />
+				<input type='hidden' name='pop_orderdatetime' value='$orderdatetime' />
+				<input type='hidden' name='pop_completedatetime' value='$completedatetime' />
+				<input type='hidden' name='pop_orderid' value='$orderid' />
+				
+				";
+		 $order_sql = "SELECT * FROM OrderDetail WHERE (OrderId = $orderid);";
+		 $order_result = mysql_query($order_sql);
+		 // Get the value of each orderdetail ordered within the given order
+		 $count = 0;
+		 $item_array = array();
+		 while($order_row = mysql_fetch_assoc($order_result))
+		 {
+			 $count++;
+			 $itemid = $order_row['ItemId'];
+			 $qty = $order_row['QuantityOrdered'];
+			 $item_sql = "SELECT Description, Size FROM InventoryItem WHERE (ItemId = $itemid);";
+			 $item_result = mysql_query($item_sql);
+			 // Get the description and size from each item in the order
+			 while($item_row = mysql_fetch_assoc($item_result))
+			 {
+				 $_id_for_next_item = "item".$count;
+				 $_description = $item_row['Description'];
+				 $_size = $item_row['Size'];
+			
+				 echo"
+				 	
+				 		<input type='hidden' name='item$count' value='$_description' />
+						<input type='hidden' size='5' name='qty$count' value='$qty' />
+					
+				 ";
+			 }
+		 }
+				
+				echo"
+				<input type='hidden' name='count' value='$count' />
 	 			<h3>Order Id: $orderid
-				 <input type='button' id='orderStaus' name='orderStaus' value='View Detail'/> </h3>
+				 <input type='submit' id='submit' name='submit' value='View Detail'>
+
+				 </form>
 	 		</div>
 	 		";
-
-
-		 echo"
-		 <table name='reportTable'>
-		 		<th colspan='3'></th>
-		 </table>
-
-
-
-		 <form action='' method='post'>
-			 <table align='center'>
-
-			 <tr>
-				 <td><p style=\"padding-right: 30px;\" align='right'>Nanno's Foods Store Location:</p></td>
-				 <td><p style=\"padding-right: 30px;\" align='left'>$storeid</p></td>
-			 </tr>
-			 <tr>
-	 			 <td><p style=\"padding-right: 30px;\" align='right'>Date and Time of Order Placement:</p></td>
-	 			 <td><p style=\"padding-right: 30px;\" align='left'>$orderdatetime</p></td>
-	 		 </tr>
-			 <tr>
-				 <td><p style=\"padding-right: 30px;\" align='right'>Date and Time of Order Fulfillment:</p></td>
-				 <td><p style=\"padding-right: 30px;\" align='left'>$completedatetime</p></td>
-			 </tr>
-			</table>
-		 ";
-
-		 echo"
+    
+		/*echo"
 			<p align='center'>-----------------------------------------------------------------------------------------------</p>
 		<table align='center'>
 
@@ -71,7 +85,7 @@ function show_order($result, $orderStatus)
 
 
 		 ";
-
+*/
 		 $order_sql = "SELECT * FROM OrderDetail WHERE (OrderId = $orderid);";
 		 $order_result = mysql_query($order_sql);
 
@@ -93,12 +107,13 @@ function show_order($result, $orderStatus)
 				 $_id_for_next_item = "item".$count;
 				 $_description = $item_row['Description'];
 				 $_size = $item_row['Size'];
+				 /*
 				 echo"
 				 	<tr>
 				 		<td><p style=\"padding-right: 30px;\" align='left'>".$_description.", ".$_size."</p></td>
 						<td><input type='text' size='5' id='".$_id_for_next_item."' value='$qty' readonly/></td>
 					</tr>
-				 ";
+				 ";*/
 			 }
 		 }
 
@@ -109,7 +124,7 @@ function show_order($result, $orderStatus)
 
 	echo "</table>";
 	echo "</form>";
-	echo "<hr/><br><br>";
+
 
 
 
